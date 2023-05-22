cb896b8b9be43663c9a8963f61391d51913b8d9e
Shweta-MG <shweta.malav@gmail.com>
2023-05-22 04:43:54 +0200

    Initial commit

---
+++ src/backend/api/meals.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const mealsRouter = express.Router();
+const knex = require("../database.js");
+
+
+
+
+// get
+mealsRouter.get("/", async (request, response) => {
+  
+  try {
+    const meals = await knex.select("*").from('meals');
+    response.json(meals);
+  } catch (error) {
+    //throw error;
+    res.status(500).json({
+      error: 'An error occurred'
+    });
+  }
+});
+
+
+
+// get
+mealsRouter.get("/:id", async (request, response) => {
+  const id = +(request.params.id);
+  
+  try {
+    const meal = await knex.select("*")
+      .from('meals')
+      .where('id', id)
+      .first();    
+    if (meal) {
+      response.json(meal);      
+    } else {
+      response.send({ "message": `Meal with  ID number ${id} does not exist` });
+    }    
+  } catch (error) {
+    //throw error;
+    res.status(500).json({
+      error: 'An error occurred'
+    });
+  }
+});
+
+
+//Post
+mealsRouter.post("/", async (request, response) => {
+  const newMeal = request.body;
+  try {
+    await knex('meals').insert(newMeal);
+    response.status(201).json(newMeal);
+  } catch (error) {
+    //throw error;
+    //It can be '400'  when log in specific rights will be considered. 
+    res.status(500).json({
+      error: 'An error occurred'
+    });
+  }
+})
+
+//Delete
+mealsRouter.delete("/:id", async (request, response) => {
+  const id = +(request.params.id);
+  
+  try {
+    const deletedMeal = await knex('meals')
+      .where('id', id)
+      .del();
+    if (deletedMeal === 1) {
+      response.send({ "message": `Meal with  ID number ${id} is deleted.` });
+    } else if (deletedMeal === 0){
+      response.send({ "message": `Meal with  ID number ${id} does not exist.` });
+    }    
+  } catch (error) {
+    //throw error;
+    //It can be '400'  when log in specific rights will be considered. 
+    res.status(500).json({
+      error: 'An error occurred'
+    });
+  }
+});
+
+
+//Put
+mealsRouter.put("/:id", async (request, response) => {
+  const updatedMeal = request.body;
+  const id = +(request.params.id);
+  try {
+    await knex('meals')
+      .where('id', id)
+      .update(updatedMeal);
+    if (!id) {
+      response.status(200).json({
+        message: `Meal with ID ${id} could not found`
+      })
+    } else {
+        response.status(200).json({
+          message: `Meal with ID ${id} updated successfully`
+        });
+
+      }
+    } catch (error) {
+    //throw error;
+    //It can be '400' - 'authantication required'  when log in 
+    //specific rights will be considered.
+    res.status(500).json({
+      error: 'An error occurred'
+    });
+  }
+});
+
+
+
+module.exports = mealsRouter;
---
+++ src/backend/api/reservations.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const reservationsRouter = express.Router();
+const knex = require("../database.js");
+
+
+
+// get
+reservationsRouter.get("/", async (request, response) => {
+  
+  try {
+    const reservations = await knex.select("*").from('reservations');
+    response.json(reservations);
+  } catch (error) {
+    //throw error;
+    res.status(500).json({
+      error: 'Internal Server Error - An error occurred while fetching data'
+    });
+  }
+});
+
+// get
+reservationsRouter.get("/:id", async (request, response) => {
+    const id = +(request.params.id);
+    
+    try {
+      const reservation = await knex.select("*")
+        .from('reservations')
+        .where('id', id)
+        .first();    
+      if (reservation) {
+        response.json(reservation);      
+      } else {
+        response.send({ "message": `Reservation with  ID number ${id} does not exist` });
+      }    
+    } catch (error) {
+      //throw error;
+      res.status(500).json({
+        error: 'Internal Server Error - An error occurred while fetching data'
+      });
+    }
+});
+  
+
+
+
+//Post
+reservationsRouter.post("/", async (request, response) => {
+    const newReservation = request.body;
+    try {
+      await knex('reservations').insert(newReservation);
+      response.json(newReservation);
+    } catch (error) {
+      //throw error;
+      //It can be '400'  when log in specific rights will be considered. 
+      res.status(500).json({
+        error: 'Internal Server Error - An error occurred while creating data'
+      });
+      
+    }
+})
+
+
+//Delete
+reservationsRouter.delete("/:id", async (request, response) => {
+    const id = +(request.params.id);
+    
+    try {
+      const deletedReservations = await knex('reservations')
+        .where('id', id)
+        .del();
+      if (deletedReservations) {
+        response.send({ "message": `Meal with  ID number ${id} is deleted.` });
+      } else {
+        response.send({ "message": `Meal with  ID number ${id} does not exist.` });
+      }    
+    } catch (error) {
+      //throw error;
+      //It can be '400'  when log in specific rights will be considered. 
+      res.status(500).json({
+        error: 'An error occurred'
+      });
+    }
+});
+
+
+//put
+reservationsRouter.put("/:id", async (request, response) => {
+    const updatedReservations = request.body;
+    const id = +(request.params.id);
+    try {
+      await knex('reservations')
+        .where('id', id)
+        .update(updatedReservations);
+      response.json(updatedReservations);
+    } catch (error) {
+      //throw error;
+      //It can be '400'  when log in specific rights will be considered. 
+      res.status(500).json({
+        error: 'An error occurred'
+      });
+    }
+});
+
+module.exports = reservationsRouter;
